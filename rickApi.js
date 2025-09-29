bc924f810c963892d14ab8fe3c2707f9a00ca27d
JOÃO PEDRO TEODORO NUNES CORREIA <25132914@CFP127ALUNO.LOCAL>
2025-09-29 10:46:04 -0300

    pequenos ajustes no css

--- rickApi.js
+++ rickApi.js
@@ -1,30 +1,51 @@
 'use strict'
 
+// const rickApi = 'https://rickandmortyapi.com/api/character/'
+// import { ApiResponse, Character, CharacterFilter, Info } from '../rickmortyapi/dist/character';
+
+import { getCharacter } from './node_modules/rickmortyapi'
+
 const rickApi = 'https://rickandmortyapi.com/api/character/'
-import { ApiResponse, Character, CharacterFilter, Info } from '../interfaces';
 
 
 
 
+const MESSAGE_ERRO = { status: false, status_code: 500, development: 'João Pedro Teodoro Nunes Correia' }
+
+
+
+function getAllPersonagens(){
 fetch(rickApi)
   .then(response => response.json())
   .then(data => {
     console.log(data)
     // processando os dados
   })
-  .catch(error => {
-    console.error('Error fetching data:', error)
+  .catch(MESSAGE_ERRO => {
+    console.error('Error fetching data:', MESSAGE_ERRO)
   })
+
+}
+
+console.log(getAllPersonagens)
+
 
 async function getPersonagemAleatorio() {
 
-  try{
     const response = await fetch(rickApi);
     const data = await response.json();
-
-    if 
-  }
+    return data.info.count
   
 }
 
-  module.exports = getAleatorio
+function getPersonagemById (totalDePersonagens){
+
+  return Math.floor(math.random() * totalDePersonagens) + 1
+
+}
+
+
+
+
+
+//   module.exports = getPersonagemAleatorio
