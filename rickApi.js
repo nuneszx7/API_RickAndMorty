65c6868974a5410f4e88b4cca3a9a6360fddd017
JOÃO PEDRO TEODORO NUNES CORREIA <25132914@CFP127ALUNO.LOCAL>
2025-09-29 09:59:14 -0300

    funções do portal feitas, iniciando função para sortear um personagem aleatório

--- rickApi.js
+++ rickApi.js
@@ -1,9 +1,7 @@
 'use strict'
 
-
-
 const rickApi = 'https://rickandmortyapi.com/api/character/'
-
+import { ApiResponse, Character, CharacterFilter, Info } from '../interfaces';
 
 
 
@@ -17,3 +15,16 @@ fetch(rickApi)
   .catch(error => {
     console.error('Error fetching data:', error)
   })
+
+async function getPersonagemAleatorio() {
+
+  try{
+    const response = await fetch(rickApi);
+    const data = await response.json();
+
+    if 
+  }
+  
+}
+
+  module.exports = getAleatorio
